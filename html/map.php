7e39ee00fff71e738de12f59828134c6df84876f
Weerayut Pimdee <forgetme_1403@hotmail.com>
2020-03-16 23:50:31 +0700

    Update map.php

--- html/map.php
+++ html/map.php
@@ -6,17 +6,21 @@
     <title>MAP</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+
 </head>
 <body>
+<div id="map" style="width: 100%; height: 400px"></div>
+
 <script>
-        var map = L.map('map').setView([-41.2858, 174.78682], 14);
-        mapLink = 
-            '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer(
-            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; ' + mapLink + ' Contributors',
-            maxZoom: 18,
-            }).addTo(map);
-    </script>
+    var map = L.map('map').setView([14.2036470, 101.2147087], 14);
+    mapLink = 
+        '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+    L.tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; ' + mapLink + ' Contributors',
+        maxZoom: 18,
+        }).addTo(map);
+</script>
+
 </body>
 </html>
