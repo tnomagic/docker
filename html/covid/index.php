c4fb0cc7faefbdfc95b8f93890265473fad08a9a
Weerayut Pimdee <forgetme_1403@hotmail.com>
2020-03-18 08:57:47 +0700

    รพ 4 แห่ง

--- html/covid/index.php
+++ html/covid/index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MAP</title>
+    <meta name="author" content="Weerayut Pimdee">
+    <title>MAP Covid Nakhon Nayok</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
 
@@ -24,15 +25,24 @@
     var hosIcon = L.icon({
     iconUrl: 'images/hospital.png',
     iconSize: [32, 32],
-    popupAnchor: [-3, -76]}
+    popupAnchor: [0, 0]
+    });
 
-    var hosIcon = L.icon({
-    iconUrl: 'images/hospital.png',
+    var ralertIcon = L.icon({
+    iconUrl: 'images/ralert.png',
     iconSize: [32, 32],
-    popupAnchor: [-3, -76]}
-});
-L.marker([14.2080062,101.2130883], {icon: hosIcon}).addTo(map); //รพ นครนายก
-L.marker([14.1779573,101.0606298], {icon: hosIcon}).addTo(map); //กองร้อยอาสา
+    popupAnchor: [0, 0]
+    });
+
+
+    L.icon = function (options) {
+    return new L.Icon(options);
+    };
+
+L.marker([14.2080062,101.2130883], {icon: hosIcon}).addTo(map).bindPopup("โรงพยาบาลนครนายก <br> เลขที่ 1/100 ถนนสุวรรณศร อำเภอเมือง จังหวัดนครนายก 26000 <br> โทรศัพท์ <a href='tel:037311151'>037-311151</a>, <a href='tel:037312440'>037-312440</a> <br> Website <a href='https://www.nayokhospital.go.th/' target='_blank'>www.nayokhospital.go.th</a> <br> <a href='https://goo.gl/maps/97MQhPw8ZzHXTBx96' target='_blank'>Google MAP</a> "); //รพ นครนายก
+L.marker([14.2523673,101.0661918], {icon: hosIcon}).addTo(map).bindPopup("โรงพยาบาลบ้านนา<br> 77 หมู่  1 ตำบลพิกุลออก อำเภอบ้านนา จังหวัดนครนายก 26110 <br> โทรศัพท์ <a href='tel:037381832'>037-381832</a> <br> Website <a href='http://www.pakpleehos.com/' target='_blank'>www.pakpleehos.com/</a> <br> <a href='https://goo.gl/maps/8cDkN5quZ3Y4cCvY8' target='_blank'>Google MAP</a> "); //รพ ปากพลี
+L.marker([14.1499467,101.2659577], {icon: hosIcon}).addTo(map).bindPopup("โรงพยาบาลปากพลี<br> 233 ตำบล ปากพลี อำเภอปากพลี นครนายก 26130 <br> โทรศัพท์ <a href='tel:037381832'>037-381832</a> <br> Website <a href='https://www.bannahospital.com/' target='_blank'>www.bannahospital.com</a> <br> <a href='https://goo.gl/maps/8cDkN5quZ3Y4cCvY8' target='_blank'>Google MAP</a> "); //รพ บ้านนา
+L.marker([14.1251332,101.0053268], {icon: hosIcon}).addTo(map).bindPopup("โรงพยาบาลองครักษ์<br> 300 หมู่ 1 ต อำเภอองครักษ์ นครนายก 26120 <br> โทรศัพท์ <a href='tel:037391511'>037-391511</a> <br> Website <a href='https://ongkharakhospital.go.th/' target='_blank'>www.ongkharakhospital.go.th/</a> <br> <a href='https://goo.gl/maps/C6h7s8vQ8fjkDmsE7' target='_blank'>Google MAP</a> "); //รพ องครักษ์
 </script>
 
 </body>
