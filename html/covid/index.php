75e01574c0c1c04a509a8772183c4002dc806539
Weerayut Pimdee <forgetme_1403@hotmail.com>
2020-03-21 17:30:09 +0700

    ci4

--- html/covid/index.php
+++ html/covid/index.php
@@ -35,10 +35,6 @@
     });
 
 
-    L.icon = function (options) {
-    return new L.Icon(options);
-    };
-
 L.marker([14.2080062,101.2130883], {icon: hosIcon}).addTo(map).bindPopup("โรงพยาบาลนครนายก <br> เลขที่ 1/100 ถนนสุวรรณศร อำเภอเมือง จังหวัดนครนายก 26000 <br> โทรศัพท์ <a href='tel:037311151'>037-311151</a>, <a href='tel:037312440'>037-312440</a> <br> Website <a href='https://www.nayokhospital.go.th/' target='_blank'>www.nayokhospital.go.th</a> <br> <a href='https://goo.gl/maps/97MQhPw8ZzHXTBx96' target='_blank'>Google MAP</a> "); //รพ นครนายก
 L.marker([14.2523673,101.0661918], {icon: hosIcon}).addTo(map).bindPopup("โรงพยาบาลบ้านนา<br> 77 หมู่  1 ตำบลพิกุลออก อำเภอบ้านนา จังหวัดนครนายก 26110 <br> โทรศัพท์ <a href='tel:037381832'>037-381832</a> <br> Website <a href='http://www.pakpleehos.com/' target='_blank'>www.pakpleehos.com/</a> <br> <a href='https://goo.gl/maps/8cDkN5quZ3Y4cCvY8' target='_blank'>Google MAP</a> "); //รพ ปากพลี
 L.marker([14.1499467,101.2659577], {icon: hosIcon}).addTo(map).bindPopup("โรงพยาบาลปากพลี<br> 233 ตำบล ปากพลี อำเภอปากพลี นครนายก 26130 <br> โทรศัพท์ <a href='tel:037381832'>037-381832</a> <br> Website <a href='https://www.bannahospital.com/' target='_blank'>www.bannahospital.com</a> <br> <a href='https://goo.gl/maps/8cDkN5quZ3Y4cCvY8' target='_blank'>Google MAP</a> "); //รพ บ้านนา
