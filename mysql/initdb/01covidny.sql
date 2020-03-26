a5a0dd3fdd5b25befdefa0547c86077e795077ec
Weerayut Pimdee <forgetme_1403@hotmail.com>
2020-03-26 12:49:49 +0700

    delete Ci3

--- mysql/initdb/01covidny.sql
+++ mysql/initdb/01covidny.sql
@@ -10,10 +10,28 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2020-03-25 22:26:02
+Date: 2020-03-26 12:27:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tb_data
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_data`;
+CREATE TABLE `tb_data` (
+  `data_id` int(255) NOT NULL AUTO_INCREMENT,
+  `co_id` varchar(20) NOT NULL COMMENT 'ละติจูด',
+  `da_name` varchar(255) DEFAULT '',
+  `da_icon` varchar(255) NOT NULL COMMENT 'ลองติจูด',
+  `da_popup` text DEFAULT NULL COMMENT 'ข้อความ',
+  `da_type` varchar(10) DEFAULT '' COMMENT 'ประเภทคนมาจากต่างจังหวัด หรือ ต่างประเทศ',
+  PRIMARY KEY (`data_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_data
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for tb_map
@@ -24,12 +42,26 @@ CREATE TABLE `tb_map` (
   `co_name` varchar(255) DEFAULT NULL,
   `co_lati` varchar(20) NOT NULL COMMENT 'ละติจูด',
   `co_longti` varchar(255) NOT NULL COMMENT 'ลองติจูด',
-  `co_icon` varchar(255) NOT NULL COMMENT 'รูปสีแดง เหลือง เขียว',
-  `co_popup` text DEFAULT NULL COMMENT 'ข้อความ',
-  `co_type` varchar(10) DEFAULT '' COMMENT 'ประเภทคนมาจากต่างจังหวัด หรือ ต่างประเทศ',
   PRIMARY KEY (`co_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_map
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_users
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_users`;
+CREATE TABLE `tb_users` (
+  `user_id` int(255) NOT NULL AUTO_INCREMENT,
+  `user_user` varchar(255) DEFAULT NULL,
+  `user_pass` varchar(255) DEFAULT NULL,
+  `user_last` datetime DEFAULT NULL,
+  `user_type` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_users
 -- ----------------------------
